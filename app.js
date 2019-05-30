b7fb2039cd386e20196f7f2d5cd319259894739b
Kaufman <alexkaufman06@gmail.com>
2019-05-30 08:35:17 -0700

    Switch from var to let variables and refactor guess function

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
 
 /** ********************* GLOBAL VARIABLES **************************** **/
 
-var employees = [];
-var employeesCopy;
-var correctAnswer;
-var correctAnswers = 0;
-var totalGuesses = 0;
-var styles = '<style>.MeetOurEmployees {text-align:center;} .MeetOurEmployees > img {border-radius: 9px; width: 150px} h2 {text-align:center !important;}.button-holder > button {display: inline-block;cursor: pointer;margin: 10px;font-weight: 400;color: #212529;border-color: #343a40 !important;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-color: transparent;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;} button:hover {color: #fff !important;background-color: #343a40;border-color: #343a40;}</style>';
-var buttons = '<div class="button-holder"><button type="button"></button><button type="button"></button><button type="button"></button><button type="button"></button></div>';
+let employees = [];
+let employeesCopy;
+let correctAnswer;
+let correctAnswers = 0;
+let totalGuesses = 0;
+let styles = '<style>.MeetOurEmployees {text-align:center;} .MeetOurEmployees > img {border-radius: 9px; width: 150px} h2 {text-align:center !important;}.button-holder > button {display: inline-block;cursor: pointer;margin: 10px;font-weight: 400;color: #212529;border-color: #343a40 !important;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-color: transparent;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;} button:hover {color: #fff !important;background-color: #343a40;border-color: #343a40;}</style>';
+let buttons = '<div class="button-holder"><button type="button"></button><button type="button"></button><button type="button"></button><button type="button"></button></div>';
 
 /** *********************** SETUP TASKS ****************************** **/
 
@@ -19,7 +19,7 @@ var buttons = '<div class="button-holder"><button type="button"></button><button
 for (i=0; i < $('.result').length; i++) {
   employees.push({name: $('.result > span')[i].innerHTML, img: $('.result > a> img')[i].src});
 }
-var employeesCopy = [...employees];
+employeesCopy = [...employees];
 
 // Add inline CSS to page
 $('#ctl00_divCenter')[0].innerHTML += styles;
@@ -30,13 +30,13 @@ $('.CustomWidget > h2')[0].innerHTML += " Quiz";
 /** ********************** HELPER FUNCTIONS *************************** **/
 
 // Remove employees from DOM (Converted to ES6 syntax)
-var removeEmployees = _ => {
+let removeEmployees = _ => {
   for (i = $('.result').length - 1; i >= 0; i--) {
     $('.result')[i].remove();
   }
 }
 
-var answersGenerator = _ => {
+let answersGenerator = _ => {
   // Choose an employee, get their data, and remove them from the employees array
   chosenNumber = Math.floor(Math.random() * employees.length);
   correctAnswer = employees[chosenNumber];
@@ -55,10 +55,10 @@ var answersGenerator = _ => {
   }
 }
 
-var renderScoreAndImg = imgSrc => $('.MeetOurEmployees > .MeetOurEmployees')[0].innerHTML = `<p>Score: ${correctAnswers}/${totalGuesses}</p><br><img src="${imgSrc}">`;
+let renderScoreAndImg = imgSrc => $('.MeetOurEmployees > .MeetOurEmployees')[0].innerHTML = `<p>Score: ${correctAnswers}/${totalGuesses}</p><br><img src="${imgSrc}">`;
 
 // Shuffle answer choices so they appear in random order
-var shuffle = a => {
+let shuffle = a => {
   for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
@@ -66,7 +66,7 @@ var shuffle = a => {
   return a;
 }
 
-var renderButtons = _ => {
+let renderButtons = _ => {
   $('.MeetOurEmployees > .MeetOurEmployees')[0].innerHTML += buttons;
   wrongAnswers[3] = correctAnswer;
   shuffle(wrongAnswers);
@@ -76,30 +76,34 @@ var renderButtons = _ => {
   }
 }
 
-var guess = name => {
-  if (name == correctAnswer.name) {
-    alert("Correct!");
-    correctAnswers += 1;
-    totalGuesses += 1;
-  } else {
-    alert(`False, it was: ${correctAnswer.name}`);
-    totalGuesses += 1;
-  }
+let correctGuess = _ => {
+  alert("Correct!");
+  correctAnswers += 1;
+  totalGuesses += 1;
+}
+
+let falseGuess = _ => {
+  alert(`False, it was: ${correctAnswer.name}`);
+  totalGuesses += 1;
+}
+
+let guess = name => {
+  (name == correctAnswer.name) ? correctGuess() : falseGuess();
   runQuiz();
 }
 
-var nextQuestion = _ => {
+let nextQuestion = _ => {
   removeEmployees();
   answersGenerator();
   renderScoreAndImg(correctAnswer.img);
   renderButtons();
 }
 
-var endQuiz = _ => {
+let endQuiz = _ => {
   $('.MeetOurEmployees > .MeetOurEmployees')[0].innerHTML = `<h3>You finished the quiz!</h3><br><p>Score: ${correctAnswers}/${totalGuesses}</p>`;
 }
 
-var runQuiz = _ => (employees.length > 0) ? nextQuestion() : endQuiz();
+let runQuiz = _ => (employees.length > 0) ? nextQuestion() : endQuiz();
 
 /** ************************ RUN QUIZ ********************************* **/
 
@@ -108,3 +112,4 @@ runQuiz();
 // NEXT STEPS:
 // Allow user to set number of questions and restart 
 // Look for opportunites to remove duplication (like the meet our employees html)
+// start filtering QA and engineering departments
